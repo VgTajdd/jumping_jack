58d011e683ea688506c467bdbe4cccd0767b96c0
VgTajdd <aduranddiaz@gmail.com>
2021-08-03 07:22:52 -0500

    Add macros helpful macros to Actor.h
    Also add virtual function update

--- Sandbox/src/world/component/Component.h
+++ Sandbox/src/world/component/Component.h
@@ -19,9 +19,23 @@ public:                               \
 private:                              \
 	type m_##var;
 
+#define DEFINE_GET_COMPONENT_INSTANCE( Type )                              \
+	static std::shared_ptr<Type> get##Type( const std::shared_ptr<Actor>& actor ) \
+	{                                                                      \
+		static std::shared_ptr<Type> instance;                             \
+		if ( instance.get() == nullptr )                                   \
+		{                                                                  \
+			instance = actor->getComponent<Type>();                        \
+		}                                                                  \
+		return instance;                                                   \
+	}
+
+#define GET_COMPONENT_INSTANCE( Type ) get##Type( actor() )
+
 class Component
 {
 public:
 	virtual ~Component(){};
+	virtual void update( float dt ){};
 	ADD_PROPERTY( std::shared_ptr<Actor>, actor )
 };
