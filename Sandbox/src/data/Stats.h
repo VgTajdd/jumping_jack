00c7be6e52084bd12fe2485a60d954ec2d28081d
VgTajdd <aduranddiaz@gmail.com>
2021-08-06 08:55:40 -0500

    Update data/Stats.h
    Add function resetStats

--- Sandbox/src/data/Stats.h
+++ Sandbox/src/data/Stats.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "Constants.h"
+
 namespace stats
 {
 static int LEVEL = 1;
 static int LIFES = 1;
 static int SCORE = 0;
 static int CURRENT_PLATFORM = 0;
+
+void resetStats()
+{
+	LEVEL = 1;
+	LIFES = constants::INITIAL_LIFES;
+	SCORE = 0;
+	CURRENT_PLATFORM = 0;
+}
 } // namespace stats
