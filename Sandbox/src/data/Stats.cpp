7f013e4b19856a9064f11c4854fc35041007ad9d
VgTajdd <aduranddiaz@gmail.com>
2021-08-10 08:06:42 -0500

    Update Stats

--- Sandbox/src/data/Stats.cpp
+++ Sandbox/src/data/Stats.cpp
@@ -1,4 +1,9 @@
 #include "Stats.h"
+#include "Constants.h"
+#include "scene/SceneManager.h"
+#include "scene/SceneType.h"
+
+#include "world/jj/JJWorld.h"
 
 namespace stats
 {
@@ -6,4 +11,42 @@ int Stats::LEVEL = 1;
 int Stats::LIFES = 1;
 int Stats::SCORE = 0;
 int Stats::CURRENT_PLATFORM = 0;
+
+void Stats::resetStats()
+{
+	Stats::LEVEL = 1;
+	Stats::LIFES = constants::INITIAL_LIFES;
+	Stats::SCORE = 0;
+	Stats::CURRENT_PLATFORM = 0;
+}
+
+void Stats::onNextLevel()
+{
+	Stats::LEVEL++;
+	Stats::CURRENT_PLATFORM = 0;
+	SceneManager::Get()->setNextScene( SCENE_TYPE::GAME );
+}
+
+void Stats::completeJump()
+{
+	Stats::CURRENT_PLATFORM++;
+	Stats::SCORE += constants::COMPLETE_JUMP_SCORE;
+	JJWorld::Get()->setScore( SCORE );
+	if ( Stats::CURRENT_PLATFORM == constants::TOTAL_PLATFORMS )
+	{
+		onNextLevel();
+	}
+}
+
+void Stats::fall()
+{
+	Stats::CURRENT_PLATFORM--;
+	Stats::LIFES--;
+	JJWorld::Get()->setLifes( LIFES );
+	if ( Stats::LIFES == 0 )
+	{
+		// Game Over.
+	}
+}
+
 } // namespace stats
--- Sandbox/src/data/Stats.h
+++ Sandbox/src/data/Stats.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include "Constants.h"
-
-#include "scene/SceneManager.h"
-#include "scene/SceneType.h"
-
 namespace stats
 {
 struct Stats
@@ -14,39 +9,12 @@ struct Stats
 	static int SCORE;
 	static int CURRENT_PLATFORM;
 
-	static void resetStats()
-	{
-		Stats::LEVEL = 1;
-		Stats::LIFES = constants::INITIAL_LIFES;
-		Stats::SCORE = 0;
-		Stats::CURRENT_PLATFORM = 0;
-	}
-
-	static void onNextLevel()
-	{
-		Stats::LEVEL++;
-		Stats::CURRENT_PLATFORM = 0;
-		SceneManager::Get()->setNextScene( SCENE_TYPE::GAME );
-	}
-
-	static void completeJump()
-	{
-		Stats::CURRENT_PLATFORM++;
-		Stats::SCORE += constants::COMPLETE_JUMP_SCORE;
-		if ( Stats::CURRENT_PLATFORM == constants::TOTAL_PLATFORMS )
-		{
-			onNextLevel();
-		}
-	}
-
-	static void fall()
-	{
-		Stats::CURRENT_PLATFORM--;
-		Stats::LIFES--;
-		if ( Stats::LIFES == 0 )
-		{
-			// Game Over.
-		}
-	}
+	static void resetStats();
+	static void onNextLevel();
+	static void completeJump();
+	static void fall();
 };
+
+// static void getPlatformConstants()
+//{}
 } // namespace stats
